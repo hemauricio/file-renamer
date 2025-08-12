f6a692c5dae8c82a6679a5063b49d2be4fe3bb93
hemauricio <github@hemauricio.com>
2025-08-12 11:17:58 -0600

    Implemented low memory option and unified method to list the directory in normal non-low-memory option

--- renamer.py
+++ renamer.py
@@ -5,61 +5,58 @@ import re
 @click.command()
 @click.argument("directory")
 @click.argument("regex")
-@click.option("--replace", "-r", "pattern", type=(str, str), multiple=True)
+@click.option("--replace", "-r", "pattern", type=(str, str))
 @click.option("--prefix", "-p", "prefix")
 @click.option("--suffix", "-s", "suffix")
 @click.option("--low-memory", "low_memory", is_flag=True)
 def rename(directory, regex, pattern, prefix, suffix, low_memory):
-    # click.echo(f"Directory: {directory}.")
-    # click.echo(f"regex: {regex}.")
-    # click.echo(f"replace: {pattern}.")
-    # click.echo(type(pattern))
-    # click.echo(f"Prefix: {prefix}.")
-    # click.echo(f"Suffix: {suffix}.")
-    # click.echo(f"Low memory: {low_memory}.")
 
     if low_memory:
         return rename_low_memory(directory, regex, pattern, prefix, suffix)
 
-    files = os.listdir(directory)
+    files = os.scandir(directory)
 
-    matches = [f for f in files if os.path.isfile(f) and re.search(regex, f) is not None]
+    matches = [file.name for file in files if file.is_file()
+                                        and re.search(regex, file.name) is not None]
     matches_copy = matches[::]
-
-    click.echo(f"Found {len(matches)} files!")
 
     if not matches:
         click.echo("No files found")
         return
-    # click.echo(matches)
 
+    # Generate new list of names in order
     if prefix:
-        click.echo(f"Adding prefix={prefix}")
         matches = [add_prefix(match, prefix) for match in matches]
 
     if suffix:
-        click.echo(f"Adding suffix={suffix}")
         matches = [add_suffix(match, suffix) for match in matches]
 
     if pattern:
-        pattern = pattern[0]
-        click.echo(f"Replacing pattern,with ={pattern}")
         matches = [replace_pattern(match, pattern) for match in matches]
 
-    # click.echo(f"New files: {matches}")
-    # click.echo(matches_copy)
-    # click.echo(matches)
-
-    # click.echo(list(map(os.rename, matches_copy, matches)))
+    # apply new names in order
     list(map(os.rename, matches_copy, matches))
 
-    click.echo("Finished")
-
-# TODO: Implement low memory option with generators os.walk
 def rename_low_memory(directory, regex, pattern, prefix, suffix):
 
-    click.echo("Welcome to the low memory option!")
+    for file in rename_lm_generator(directory, regex):
+        new_name = file.name
 
+        if prefix:
+            new_name = add_prefix(new_name, prefix)
+
+        if suffix:
+            new_name = add_suffix(new_name, suffix)
+
+        if pattern:
+            new_name = replace_pattern(new_name, pattern)
+
+        os.rename(file.path, new_name)
+
+def rename_lm_generator(directory, regex):
+    for file in os.scandir(directory):
+        if file.is_file() and re.search(regex, file.name) is not None:
+            yield file
 
 def add_prefix(filename, prefix):
     return prefix + filename
